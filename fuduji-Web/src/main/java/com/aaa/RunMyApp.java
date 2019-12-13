862a98fda08392370bd2d0ad3381c4285bc0cd84
J-hsitsung <2019447337>
2019-12-13 17:49:55 +0800

    提交忽略文件

---
+++ fuduji-Web/src/main/java/com/aaa/RunMyApp.java
@@ -0,0 +1,18 @@
+package com.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: RunMyApp
+ * @Description:
+ * @Author: J-Hsitsung.s
+ * @Date 2019/12/13 17:19
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class RunMyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RunMyApp.class);
+    }
+}
